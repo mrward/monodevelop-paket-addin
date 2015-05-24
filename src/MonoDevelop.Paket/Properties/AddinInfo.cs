62fddcfd8893d417b068cc1104da183f7a1cf61c
Matt Ward <ward.matt@gmail.com>
2015-05-24 10:33:52 +0100

    Create Paket addin for MonoDevelop.
    Adds syntax highlighting for paket.dependencies file.
    Adds a file template for paket.dependencies.

---
+++ src/MonoDevelop.Paket/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+
+[assembly:Addin ("Paket",
+	Namespace = "MonoDevelop",
+	Version = "0.1",
+	Category = "IDE extensions")]
+
+[assembly:AddinName ("Paket")]
+[assembly:AddinDescription ("Adds Paket support.")]
+
+[assembly:AddinDependency ("Core", "5.0")]
+[assembly:AddinDependency ("Ide", "5.0")]
+[assembly:AddinDependency ("SourceEditor2", "5.0")]
